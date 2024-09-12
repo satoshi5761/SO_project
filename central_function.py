ccdbf8a027fdd6443ccacd274016d8ed79cf3fee
philip <philip.deric@ti.ukdw.ac.id>
2024-09-12 19:58:36 +0700

    kentut

--- central_function.py
+++ central_function.py
@@ -3,19 +3,37 @@ import os as so
 class LinuxOS:
     def __init__ (self):
         # list of necessary Linux commands
-        self.ls_dir = "ls -d */"
-        self.cd = "cd"
 
-    def get_current_directories(self):
+        # menangkap semua directory di 
+        # path saat ini
+        self.ls_dir = "ls -d */"
+
+        # menampilkan path saat ini
+        self.pwd = "pwd"
+
+    def show_current_path(self):
+        """
+        menampilkan path directory saat ini
+        """
+        run_command = so.popen(self.pwd)
+        read_command = run_command.read().rstrip()
+        print(read_command)
+
+
+    def get_available_directories(self):
+        """
+        return semua directory pada working directory jika ada
+        return empty [] jika tidak ada
+        """
         run_command = so.popen(self.ls_dir)
         read_command = run_command.read().rstrip()
         lst_directories = read_command.split('\n')
 
-        print(lst_directories)
+        if ''.join(lst_directories) == "":
+            return []
+        else:
+            return lst_directories
 
-class WindowsOS:
-    def __init__ (self):
-        pass
-
-debian = LinuxOS()
-debian.get_current_directories()
+    
+    def change_directory(self):
+        
--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-import os
+from central_function import LinuxOS
+
+# tambahkan utility masing-masing
+nama_utilities = (
+    "show current path",
+    "display directories",
+
+
+    "QUIT"
+)
+
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+def display_utilities():
+    """
+    menampilkan utilities yang tersedia
+    """
+    for (nomor_opsi, nama_util) in enumerate(nama_utilities):
+        print(f"Option {nomor_opsi + 1}: {nama_util}")
+
+###########################################################################
+###########################################################################
+###########################################################################
+
 
 def main():
-    print(os.name)
+
+    UBUNTU = LinuxOS()
+
+
+    Mahastama = True
+    while Mahastama:
+        display_utilities()        
+
+        try:
+            pilihan_utility = int(input("Pick an option: "))
+        except:
+            pilihan_utility = 0
+        
+
+        if pilihan_utility == 1:
+            UBUNTU.show_current_path()
+        elif pilihan_utility == 2:
+            print(UBUNTU.get_available_directories())
+        else:
+            Mahastama = False
+
+
+        print("-" * 50)
 
 main()
-
-def fibonacci(n):
-    if n == 0:
-        return 0
-    if n <= 2:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
