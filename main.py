20658779bcc51e48d4c23ec2f4d0a85445799f31
nicholasteslaaa <143601631+nicholasteslaaa@users.noreply.github.com>
2024-09-12 10:02:23 +0700

    submit nicho 1

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ def main():
     print(os.name)
 
 main()
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n <= 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
